5d10e0b0fac4a1f75604c30d968257bcf95c15e2
vanesagladisya <vanesagladisya@gmail.com>
2025-11-22 16:24:45 +0800

    Add files via upload

---
+++ 202412021_LatihanPraktikum_class_object.py
@@ -0,0 +1,16 @@
+class Dosen:
+    def __init__(self, nama, nidn):
+        self.nama = nama
+        self.nidn = nidn
+
+    def ajar_mata_kuliah(self, mata_kuliah):
+        return f"Saya {self.nama} (NIDN: {self.nidn}) mengajar mata kuliah {mata_kuliah}"
+    
+
+# Membuat object dosen
+dsn1 = Dosen("Ir. ABADI NUGROHO, S.Kom., M.Kom", "1104129002")
+dsn2 = Dosen("Lapu Tombilayuk, S.Kom., M.T.", "1120107301")
+
+# Memanggil method
+print(dsn1.ajar_mata_kuliah("PEMROGRAMAN BERORIENTASI OBJEK"))
+print(dsn2.ajar_mata_kuliah("SISTEM OPERASIK"))
